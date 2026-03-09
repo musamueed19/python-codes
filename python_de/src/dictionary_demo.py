a8b1ed8cb039f9535a7575543aba3b1d32ad3f50
Muhammad Musa Mueed <musavuswh@gmail.com>
2026-03-10 00:01:06 +0500

    dict demo file

---
+++ python_de/src/dictionary_demo.py
@@ -0,0 +1,31 @@
+raw_api_response = {
+    "order_id": 101,
+    "customer": {
+        "name": "Muhammad Musa Mueed",
+        "cust_id": 219
+    },
+    "items": [
+        {
+            "sku": "P102",
+            "price": 149
+        },
+        {
+            "sku": "P456",
+            "price": 89
+        },
+    ],
+    "country": "PAK"
+}
+
+
+
+structured_order = {
+    "order_id": raw_api_response["order_id"],
+    "customer_id": raw_api_response["customer"]["cust_id"],
+    "customer_name": raw_api_response["customer"]["name"],
+    "order_items": raw_api_response["items"],
+    "total_amount": sum(item["price"] for item in raw_api_response["items"]),
+    "order_origin_country": raw_api_response["country"]
+}
+
+print(structured_order)
