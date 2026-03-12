09e0dd515726ebfab817e67b85a067b0244d607c
Muhammad Musa Mueed <musavuswh@gmail.com>
2026-03-12 21:47:59 +0500

    added 2nd order record

--- python_de/src/data/orders_data.py
+++ python_de/src/data/orders_data.py
@@ -11,5 +11,23 @@ orders_data = [
                 "name": "Product 1"
             }
         ]
-    }
+    },
+    {
+        "id": "order_002",
+        "customer": {
+            "name": "Jane Smith"
+        },
+        "total": "150",
+        "items": [
+            {
+                "id": "item_002",
+                "name": "Product 2"
+            },
+            {
+                "id": "item_003",
+                "name": "Product 3"
+            }
+        ]
+    },
+    
 ]
